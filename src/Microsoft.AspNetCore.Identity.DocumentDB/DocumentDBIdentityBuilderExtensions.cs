50d7cedc11639764562c479349a5b87e14d39d8b
Felix Schröter <felschr@gmail.com>
2016-11-17 18:52:17 +0100

    update service registration handling
    removes MongoDB references
    allows custom collection names

--- src/Microsoft.AspNetCore.Identity.DocumentDB/DocumentDBIdentityBuilderExtensions.cs
+++ src/Microsoft.AspNetCore.Identity.DocumentDB/DocumentDBIdentityBuilderExtensions.cs
@@ -12,8 +12,8 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class DocumentDBIdentityBuilderExtensions
 	{
 		/// <summary>
-		///     This method only registers mongo stores, you also need to call AddIdentity.
-		///     Consider using AddIdentityWithMongoStores.
+		///     This method only registers DocumentDB stores, you also need to call AddIdentity.
+		///     Consider using AddIdentityWithDocumentDBStores.
 		/// </summary>
 		/// <param name="builder"></param>
 		/// <param name="connectionString">Must contain the database name</param>
@@ -26,21 +26,16 @@ namespace Microsoft.Extensions.DependencyInjection
 				throw new ArgumentException("You must reference an initialized DocumentClient");
 			}
 
-            // create collections if not exist.
-            var users = documentClient.CreateDocumentCollectionQuery(databaseLink).Where(c => c.Id.Equals("users")).AsEnumerable().FirstOrDefault()
-                ?? documentClient.CreateDocumentCollectionAsync(databaseLink, new DocumentCollection { Id = "users" }).Result;
-            var roles = documentClient.CreateDocumentCollectionQuery(databaseLink).Where(c => c.Id.Equals("roles")).AsEnumerable().FirstOrDefault()
-                ?? documentClient.CreateDocumentCollectionAsync(databaseLink, new DocumentCollection { Id = "roles" }).Result;
-
             return builder.RegisterDocumentDBStores<TUser, TRole>(
                 documentClient,
-                p => users,
-                p => roles);
+                databaseLink,
+                "users",
+                "roles");
         }
 
 		/// <summary>
 		///     If you want control over creating the users and roles collections, use this overload.
-		///     This method only registers mongo stores, you also need to call AddIdentity.
+		///     This method only registers DocumentDB stores, you also need to call AddIdentity.
 		/// </summary>
 		/// <typeparam name="TUser"></typeparam>
 		/// <typeparam name="TRole"></typeparam>
@@ -49,32 +44,35 @@ namespace Microsoft.Extensions.DependencyInjection
 		/// <param name="rolesCollectionFactory"></param>
 		public static IdentityBuilder RegisterDocumentDBStores<TUser, TRole>(this IdentityBuilder builder,
             DocumentClient documentClient,
-            Func<IServiceProvider, DocumentCollection> usersCollectionFactory,
-			Func<IServiceProvider, DocumentCollection> rolesCollectionFactory)
+            string databaseLink,
+            string userCollectionName,
+            string roleCollectionName)
 			where TRole : IdentityRole
 			where TUser : IdentityUser
 		{
 			if (typeof(TUser) != builder.UserType)
 			{
-				var message = "User type passed to RegisterMongoStores must match user type passed to AddIdentity. "
-				              + $"You passed {builder.UserType} to AddIdentity and {typeof(TUser)} to RegisterMongoStores, "
+				var message = "User type passed to RegisterDocumentDBStores must match user type passed to AddIdentity. "
+				              + $"You passed {builder.UserType} to AddIdentity and {typeof(TUser)} to RegisterDocumentDBStores, "
 				              + "these do not match.";
 				throw new ArgumentException(message);
 			}
 			if (typeof(TRole) != builder.RoleType)
 			{
-				var message = "Role type passed to RegisterMongoStores must match role type passed to AddIdentity. "
-				              + $"You passed {builder.RoleType} to AddIdentity and {typeof(TRole)} to RegisterMongoStores, "
+				var message = "Role type passed to RegisterDocumentDBStores must match role type passed to AddIdentity. "
+                              + $"You passed {builder.RoleType} to AddIdentity and {typeof(TRole)} to RegisterDocumentDBStores, "
 				              + "these do not match.";
 				throw new ArgumentException(message);
 			}
-			builder.Services.AddSingleton<IUserStore<TUser>>(p => new UserStore<TUser>(documentClient, usersCollectionFactory(p)));
-			builder.Services.AddSingleton<IRoleStore<TRole>>(p => new RoleStore<TRole>(documentClient, rolesCollectionFactory(p)));
-			return builder;
+
+			builder.Services.AddSingleton<IUserStore<TUser>>(p => new UserStore<TUser>(documentClient, ReadOrCreateCollection(documentClient, databaseLink, userCollectionName)));
+			builder.Services.AddSingleton<IRoleStore<TRole>>(p => new RoleStore<TRole>(documentClient, ReadOrCreateCollection(documentClient, databaseLink, roleCollectionName)));
+
+            return builder;
 		}
 
 		/// <summary>
-		///     This method registers identity services and MongoDB stores using the IdentityUser and IdentityRole types.
+		///     This method registers identity services and DocumentDB stores using the IdentityUser and IdentityRole types.
 		/// </summary>
 		/// <param name="services"></param>
 		/// <param name="connectionString">Connection string must contain the database name</param>
@@ -85,7 +83,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
 		/// <summary>
 		///     This method allows you to customize the user and role type when registering identity services
-		///     and MongoDB stores.
+		///     and DocumentDB stores.
 		/// </summary>
 		/// <typeparam name="TUser"></typeparam>
 		/// <typeparam name="TRole"></typeparam>
@@ -98,5 +96,11 @@ namespace Microsoft.Extensions.DependencyInjection
 			return services.AddIdentity<TUser, TRole>()
 				.RegisterDocumentDBStores<TUser, TRole>(documentClient, databaseLink);
 		}
-	}
+
+        private static DocumentCollection ReadOrCreateCollection(DocumentClient documentClient, string databaseLink, string collectionName)
+        {
+            return documentClient.CreateDocumentCollectionQuery(databaseLink).Where(c => c.Id.Equals(collectionName)).AsEnumerable().FirstOrDefault()
+                        ?? documentClient.CreateDocumentCollectionAsync(databaseLink, new DocumentCollection { Id = collectionName }).Result;
+        }
+    }
 }
